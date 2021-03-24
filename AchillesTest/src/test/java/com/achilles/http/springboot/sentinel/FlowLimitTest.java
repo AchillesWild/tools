77e26963f3673bc11c7f9ab846a023e784ae5f53
AchillesWild <532398999@qq.com>
2021-03-24 18:09:10 +0800

    test

--- AchillesTest/src/test/java/com/achilles/http/springboot/sentinel/FlowLimitTest.java
+++ AchillesTest/src/test/java/com/achilles/http/springboot/sentinel/FlowLimitTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 public class FlowLimitTest extends SpringbootApplicationTests {
@@ -17,17 +19,24 @@ public class FlowLimitTest extends SpringbootApplicationTests {
     @Test
     public void flowTest() throws Exception{
 
+        List<Long> list = new ArrayList<>();
         int max = 10;
         CountDownLatch count = new CountDownLatch(max);
+        long startTime = System.currentTimeMillis();
         for (int i = 0; i < max; i++) {
             final int m=i;
             new Thread(()->{
+                long threadStartTime = System.currentTimeMillis();
                 String result = HttpGetUtil.get(url+m,null);
+                long duration = System.currentTimeMillis() - threadStartTime;
+                list.add(duration);
                 log.info("-----------------"+m+"------------------"+result);
                 count.countDown();
             }).start();
         }
         count.await();
-        log.info("-----------------over----------------");
+        long duration = System.currentTimeMillis() - startTime;
+        log.info("---------------------------------duration : "+duration);
+        log.info("-----------------over----------------list : "+list);
     }
 }
